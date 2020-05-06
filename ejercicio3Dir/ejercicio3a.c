8b6f8c0d87e17317c3b5409f96ee05d082ecc832
Facundo Decena <facundo.decena@gmail.com>
2020-05-06 12:46:46 -0300

    Memory management

--- ejercicio3Dir/ejercicio3a.c
+++ ejercicio3Dir/ejercicio3a.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <assert.h>
 
 /**
  * Creates a array representing the matrix used in the multiplication
@@ -8,7 +9,7 @@
  * @return
  */
 
-int multiply(int column[], int vector[], int size){
+int multiply(int *column, int *vector, int size){
     int res = 0;
     int i;
     for( i = 0; i < size; i++){
@@ -30,10 +31,10 @@ int main(int argc, char** argv){
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     num_elem = size * size;
-    int matrix[num_elem];
-    int vector[size];
+    int *matrix = (int *)malloc(sizeof(int) * num_elem);
+    int *vector = (int *)malloc(sizeof(int) * size);
     // For each process create a buffer to hold the respective column
-    int column[size];
+    int *column = (int *)malloc(sizeof(int) * size);
 
     if (rank == 0){
         for(i = 0; i < num_elem; i++){
@@ -51,18 +52,30 @@ int main(int argc, char** argv){
 
     int res = multiply(column, vector, size);
 
-    int sub_total[6] = {0,0,0,0,0,0};
+    int *sub_total = NULL;
+    if(rank == 0){
+        sub_total = (int *)malloc(sizeof(int) * size);;
+    }
 
     MPI_Gather(&res, 1, MPI_INT, sub_total, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     if(rank == 0){
-
         printf("El vector resultado es: \n");
         for( i = 0; i < size; i++){
             printf("%d \n", sub_total[i]);
         }
     }
 
+    // Cleaning memory
+
+    if(rank == 0){
+        free(matrix);
+        free(vector);
+        free(sub_total);
+    }
+    free(column);
+
+    MPI_Barrier(MPI_COMM_WORLD);
     MPI_Finalize();
 
 }
